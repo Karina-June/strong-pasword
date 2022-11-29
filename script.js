49957d01b2e8bd8b4deefaf29d8efd2bdecc032d
Karina June <116246298+KarinaJune@users.noreply.github.com>
2022-11-28 18:23:19 -0600

    updated pop up alert

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-// Assignment code here
+// Listed below are all the available charaters for the password generator
 var lowercase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 var uppercase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharaters = ["!", "@", "#", "$", "%", "^", "<", ">", "&", "*", "?", "+", "="];
-//var passwordLength = ["8 to 128"];
+var passwordLength = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
 
-
+//Listed below are all the alert questions popping up at the top of the page
 function getPasswordCriteria() {
   var userChoseLowercase = confirm("Do you want lowercase letters in your password? ");
   var userChoseUppercase = confirm("Do you want uppercase letters in your password? ");
   var userChoseNumbers = confirm("Do you want numbers in your password?");
-  var userSpecialCharaters = confirm("Do you want special charaters in your password? ");
-  //var passwordLength = confirm("")
+  var userChoseSpecialCharaters = confirm("Do you want special charaters in your password? ");
+  var userChosePasswordLength = prompt("How long will your password be?");
 
   var options = {
     userChoseLowercase: userChoseLowercase,
     userChoseUppercase: userChoseUppercase,
     userChoseNumbers: userChoseNumbers,
-    userChoseSpecialCharaters: userSpecialCharaters
+    userChoseSpecialCharaters: userChoseSpecialCharaters,
+    userChosePasswordLength: userChosePasswordLength
   };
   return options;
 }
@@ -25,6 +26,7 @@ function createRandom(length) {
  var randomNum = Math.floor(Math.random() * length);
  return randomNum;
 }
+
 
 function generatePassword() {
   var userChose = getPasswordCriteria();
@@ -44,12 +46,13 @@ function generatePassword() {
   if(userChose.userChoseSpecialCharaters) {
     availableChars = availableChars.concat(specialCharaters);
   }
-
-  for(var i = 8; i < 129; i++){
-    passwordArr.push(availableChars[createRandom(availableChars.length)])
+  if(userChose.userChosePasswordLength) {
+    availableChars = availableChars.concat(passwordLength);
   }
 
-
+  for(var i = 0; i < 128; i++){
+    passwordArr.push(availableChars[createRandom(availableChars.length)])
+  }
 
   return passwordArr.join("")
 }
